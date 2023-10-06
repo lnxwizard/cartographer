05e98ae509355ecb9b22fe061e9bf38f0e56bb1a
lnxwizard <alperakca79@outlook.com>
2023-10-06 22:29:20 +0300

    `colored` flag renamed to `color`

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 
-#[derive(Parser, Debug)]
+#[derive(Parser)]
+#[command(name = "cartographer", bin_name = "crt")]
 #[command(about = "A command-line tool built with Rust for generating and saving Minecraft seeds.")]
 #[command(
     long_about = "Generate random Minecraft seeds and save favorite seeds with `cartographer`."
@@ -8,6 +9,6 @@ use clap::Parser;
 #[command(author, version)]
 pub struct Cli {
     #[arg(help = "Toggle colored output")]
-    #[arg(short = 'c', long = "colored")]
-    pub colored: bool,
+    #[arg(short = 'c', long = "color")]
+    pub color: bool,
 }
